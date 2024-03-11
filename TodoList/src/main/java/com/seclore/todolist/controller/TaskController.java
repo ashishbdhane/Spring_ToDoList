675240681f90564b4e819460168f135071a37638
unknown <Admin@RohitD19252.seclore.com>
2024-03-11 17:23:13 +0530

    Added All Tasks Page

--- TodoList/src/main/java/com/seclore/todolist/controller/TaskController.java
+++ TodoList/src/main/java/com/seclore/todolist/controller/TaskController.java
@@ -1,27 +1,55 @@
 package com.seclore.todolist.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.seclore.todolist.domain.TaskDetails;
+import com.seclore.todolist.domain.UserDetails;
+import com.seclore.todolist.service.TaskDetailsServiceInterface;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/tasks")
 public class TaskController {
 	
+	@Autowired
+	private TaskDetailsServiceInterface taskService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView showTodoListPage() {
-		
+	public ModelAndView showTodoListPage(HttpSession session) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("alltasks");
+		UserDetails userDetails = (UserDetails) session.getAttribute("loggedInUser");
+		List<TaskDetails> allTasks = taskService.getAllTaskOfUser(userDetails);
+		modelAndView.addObject("allTasks", allTasks);
+			
+		return modelAndView;
 	}
 	
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public ModelAndView showAddTaskPage() {
-		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("addtask");
+		TaskDetails taskDetails = new TaskDetails();
+		modelAndView.addObject("taskDetails", taskDetails);
+			
+		return modelAndView;
 	}
 	
 	@RequestMapping(value = "/edit", method = RequestMethod.GET)
 	public ModelAndView showEditTaskPage() {
-		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("addtask");
+		TaskDetails taskDetails = taskService.getTaskById();
+		modelAndView.addObject("taskDetails", taskDetails);
+			
+		return modelAndView;
 	}
 	
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
