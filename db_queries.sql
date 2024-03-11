112054780026761b504c88292b9dc42faa5ca27f
Ashish Dhane <81252055+ashishbdhane@users.noreply.github.com>
2024-03-11 15:50:58 +0530

    Update db_queries.sql

--- db_queries.sql
+++ db_queries.sql
@@ -3,15 +3,16 @@ create database todo_db
 use todo_db
 
 
+
 CREATE TABLE user_details(
 	user_id INT IDENTITY,
 	name VARCHAR(20) NOT NULL,
 	mobile_no VARCHAR(10) NOT NULL,
-	email TEXT NOT NULL,
+	email VARCHAR(20) NOT NULL,
 	password VARCHAR(20) NOT NULL,
 	CONSTRAINT user_id_pk PRIMARY KEY(user_id),
+	CONSTRAINT email_unique UNIQUE(email),
 )
-
 
 CREATE TABLE task_details(
 	task_id INT IDENTITY,
@@ -19,6 +20,7 @@ CREATE TABLE task_details(
 	title TEXT NOT NULL,
 	description TEXT,
 	status VARCHAR(12) NOT NULL,
+	CONSTRAINT task_id_pk PRIMARY KEY(task_id),
 	CONSTRAINT task_user_fk FOREIGN KEY(user_id) REFERENCES user_details(user_id),
 	CONSTRAINT check_status CHECK(status in ('PENDING', 'COMPLETED', 'INPROGRESS')),
 )
